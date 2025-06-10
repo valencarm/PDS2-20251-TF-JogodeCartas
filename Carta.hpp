9b85ba06d6bf059c0b33fbf4b415e250a9cf59a2
grrs42 <63813889+grrs42@users.noreply.github.com>
2025-06-09 22:19:20 -0300

    Add files via upload
    Primeiro commit com apenas o contrato de Carta.hpp

---
+++ Carta.hpp
@@ -0,0 +1,25 @@
+//Criar um baralho de Truco
+//Embaralhar, organizar e distribuir as cartas
+//Lidar com ações especiais
+//Definir quem embaralha e repor cartas
+
+//RESPONSABILIDADES : ARMAZENAR VsALOR E NAIPE, COMPARAR C/ OUTRAS CARTAS, VALIDAR JOGADA, RETORNAR IMAGEM DA CARTA
+
+#ifndef CARTA
+#define CARTA
+#include <iostream>
+#include <string>
+class Carta {
+    private: 
+    std::string naipe;
+    std::string valor;
+   
+    public: 
+    Carta(const std::string& naipe, const std::string& valor);
+    
+    std::string getCarta() const; // aí no cpp vai dar "return valor + " de " naipe;"
+    void comparaCarta(const Carta& card, Carta& outra);
+    bool validaJogada(const Carta& card, Carta& outra);
+    ~Carta(){}
+};
+#endif
